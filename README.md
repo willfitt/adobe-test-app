4882f6ca3309393d8552736bc4e119872e72ee10
William Fitt <wmfitt@hotmail.com>
2025-01-20 15:08:56 -0700

    create base app using create-react-app

---
+++ README.md
@@ -0,0 +1,13 @@
+### How to build and run this project
+
+
+
+### Engineering and testing methodology
+
+
+
+### Packaging Layout
+
+
+
+### Dependency attribution
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Edit <code>src/App.js</code> and save to reload.
+      </header>
+    </div>
+  );
+}
+
+export default App;
